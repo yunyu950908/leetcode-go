3ba7878a6206cefb26a37890bc5e2fb9d70a7dd2
yunyu950908 <yunyu950908@gmail.com>
2019-03-22 00:01:13 +0800

    feat(66): plus one

---
+++ src/golang/66_plus_one/66_plus_one.go
@@ -0,0 +1,20 @@
+package main
+
+func plusOne(digits []int) []int {
+	carry := 1
+	idx := len(digits) - 1
+	for ; carry == 1 && idx >= 0; idx -= 1 {
+		if carry == 1 {
+			digits[idx] += 1
+			carry = 0
+		}
+		if digits[idx] > 9 {
+			carry = 1
+		}
+		digits[idx] = digits[idx] % 10
+	}
+	if carry == 1 {
+		return append([]int{1}, digits...)
+	}
+	return digits
+}
