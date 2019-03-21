49c2e8b364b48ee0c4fd7f3ca948b66a55c82065
yunyu950908 <yunyu950908@gmail.com>
2019-03-21 23:32:34 +0800

    feat(747): Largest Number At Least Twice of Others

---
+++ src/golang/747_largest_number-at-least-twice-of-others/747_largest_number-at-least-twice-of-others.go
@@ -0,0 +1,19 @@
+package leetcode
+
+func dominantIndex(nums []int) int {
+	max, second, idx := 0, 0, 0
+	for i, v := range nums {
+		if v > max {
+			idx, max = i, v
+		}
+	}
+	for i, v := range nums {
+		if v > second && i != idx {
+			second = v
+		}
+	}
+	if max >= second*2 {
+		return idx
+	}
+	return -1
+}
