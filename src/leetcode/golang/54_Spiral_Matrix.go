30f143114b3b601c8f70a058de8eeab86494c786
yunyu950908 <yunyu950908@gmail.com>
2019-03-23 17:54:41 +0800

    update(golang/54): Spiral Matrix

--- src/leetcode/golang/54_Spiral_Matrix.go
+++ src/leetcode/golang/54_Spiral_Matrix.go
@@ -4,49 +4,27 @@ func spiralOrder(matrix [][]int) []int {
 	if !(len(matrix) != 0 && len(matrix[0]) != 0) {
 		return []int{}
 	}
-	ir, ic := 0, 0
+	var ir, ic, nr, nc int
 	lr, lc := len(matrix), len(matrix[0])
 	result := make([]int, lr*lc)
-	// 1 (left -> right),
-	// 2 (up -> down),
-	// 3 (right -> left),
-	// 4 (down -> up)
-	direction := 1
-	boundary := ic
+	// init seen
+	seen := make([][]bool, lr)
+	for i := range seen {
+		seen[i] = make([]bool, lc)
+	}
+	// direction
+	dr := [4]int{0, 1, 0, -1}
+	dc := [4]int{1, 0, -1, 0}
+	di := 0
 	for i := range result {
-		switch direction {
-		case 1:
-			result[i] = matrix[ir][boundary]
-			boundary += 1
-			if boundary == lc {
-				direction = 2
-				ir += 1
-				boundary = ir
-			}
-		case 2:
-			result[i] = matrix[boundary][lc-1]
-			boundary += 1
-			if boundary == lr {
-				direction = 3
-				lc -= 1
-				boundary = lc - 1
-			}
-		case 3:
-			result[i] = matrix[lr-1][boundary]
-			boundary -= 1
-			if boundary == ic-1 {
-				direction = 4
-				lr -= 1
-				boundary = lr - 1
-			}
-		case 4:
-			result[i] = matrix[boundary][ic]
-			boundary -= 1
-			if boundary == ir-1 {
-				direction = 1
-				ic += 1
-				boundary = ic
-			}
+		result[i] = matrix[ir][ic]
+		seen[ir][ic] = true
+		nr, nc = ir+dr[di], ic+dc[di]
+		if nr >= 0 && nr < lr && nc >= 0 && nc < lc && !seen[nr][nc] {
+			ir, ic = nr, nc
+		} else {
+			di = (di + 1) % 4
+			ir, ic = ir+dr[di], ic+dc[di]
 		}
 	}
 	return result
